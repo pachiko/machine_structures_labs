6ecf9338692acbc6274874b4871bdd344b6e61a7
pachiko <phillippang1994@gmail.com>
2023-01-27 22:23:25 +0800

    lab03

--- lab03/discrete_fn.s
+++ lab03/discrete_fn.s
@@ -5,7 +5,10 @@
 # a1 is the address of the "output" array (defined above).
 # The return value should be stored in a0
 f:
-
+    addi t0, a0, 3 # why on earth do we have negative index?
+    slli t0, t0, 2 # multiply the offset index by 4 bytes (2^2)
+    add t0, t0, a1 # offset cannot be register, so apply it directly on the address
+    lw a0, 0(t0) # "zero" offset
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra  
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,33 +1,41 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 4 # n!
 
 .text
 # Don't worry about understanding the code in main
 # You'll learn more about function calls in lecture soon
 main:
-    la t0, n
-    lw a0, 0(t0)
+    la t0, n # load the address of n into t0
+    lw a0, 0(t0) # dereference n
     jal ra, factorial
 
-    addi a1, a0, 0
-    addi a0, x0, 1
+    addi a1, a0, 0 # load the result
+    addi a0, x0, 1 # print integer
     ecall # Print Result
 
-    addi a1, x0, '\n'
-    addi a0, x0, 11
+    addi a1, x0, '\n' # load the char
+    addi a0, x0, 11 # print char
     ecall # Print newline
 
-    addi a0, x0, 10
+    addi a0, x0, 10 # quit
     ecall # Exit
 
 # factorial takes one argument:
 # a0 contains the number which we want to compute the factorial of
 # The return value should be stored in a0
 factorial:
-    # YOUR CODE HERE
+    add t0, x0, a0 # init counter with n
+    addi a0, x0, 1 # init result with 1
+    
+    loop:
+        beq t0, x0, done # while n > 0
+        mul a0, a0, t0 # res = res * n
+        addi t0, t0, -1 # n--
+        j loop
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
-    jr ra
+    done:
+        jr ra
